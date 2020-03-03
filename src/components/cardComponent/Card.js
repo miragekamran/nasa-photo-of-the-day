ad128b74684454aa9d1959814f5ca53959843cd0
Mirage Kamran <55200336+miragekamran@users.noreply.github.com>
2020-03-02 22:07:59 -0800

    step 3 almost finished

---
+++ src/components/cardComponent/Card.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function Card() {
+    const [data, setData] = useState({})
+    useEffect(() => {
+        axios
+        .get('https://api.nasa.gov/planetary/apod?api_key=miI40eqMuxQHfzdZnREccFeTZflbRiOwARDCDhwI')
+        .then(response => setData(response.data))
+    }, [])
+
+    console.log(data)
+    return (
+        <div className = 'wrapper'>
+            <h1>{data.title}</h1>
+            <h3>{data.date}</h3>
+            <img alt={data.title} src={data.url}/>
+            <h3>{data.copyright}</h3>
+            <p>{data.explanation}</p>
+        </div>
+    )
+}
+
+export default Card
